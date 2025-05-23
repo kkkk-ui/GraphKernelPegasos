438199a0f05091b45a5b1a74958edb60deb71648
kota <kota160815@gmail.com>
2025-05-23 17:57:18 +0900

    update

--- GraphKernelPegasosmain.py
+++ GraphKernelPegasosmain.py
@@ -11,5 +11,14 @@ myData.makeData(dataType=int(input("data type =>")))
 
 #-------------------
 # Learning and evaluation.
+iter = int(input("iter = "))
+lamda = float(input("lamda ="))
 
+Pegasos = pg.Pegasos(myData.graphs, myData.classes, iter, lamda)
+
+alpha = Pegasos.train()
+
+acc = Pegasos.accuracy(alpha)
+
+print(acc)
 
--- pegasos_evaluation.py
+++ pegasos_evaluation.py
@@ -1,10 +1,56 @@
 import numpy as np
-import GraphKernelFunc as kf
+import GraphKernelFunc as gkf
+from sklearn.model_selection import KFold
 
+class Pegasos():
+    def __init__(self, graphs, classes, iter, lamda):
+        self.graphs = graphs
+        self.classes = classes
+        self.iter = iter
+        self.lamda = lamda
 
-def pegasos(graphs, classes, iter, lamda):
-    alpha = np.zeros(len(graphs))
+    def train(self):
+        # learning
+
+        # initialize
+        alpha = np.zeros(len(self.graphs))
+
+        # iterate
+        for t in range(1,self.iter+1):
+            print("t = ",t)
+            i_t = np.random.randint(len(self.graphs))
+            sigma_loss = 0
+
+            for j in range(len(self.graphs)):
+                sigma_loss += alpha[j] * self.classes[i_t] * gkf.GraghkernelFunc.k_func_wl(self.graphs[i_t], self.graphs[j], 2)
+
+            if(self.classes[i_t] / (self.lamda * t) * sigma_loss < 1):
+                alpha[i_t] += 1
+
+        return alpha
     
-    for t in range(iter):
-        i_t = np.random.randint(len(graphs))
+    def predict(self, index, alpha):
+        # predict
+        predict = []
+        for i in index:
+            print("index = ",i)
+            y = 0
+            for j in range(len(self.graphs)):
+                y +=  alpha[j] * self.classes[j] * gkf.GraghkernelFunc.k_func_wl(self.graphs[i], self.graphs[j], 2)
+            predict.append(y)
+
+        return predict
+
+    def accuracy(self, alpha):
+        # accuracy
+        picked = np.random.choice(len(self.classes), size=500, replace=False)
+        predict = self.predict(picked, alpha)
+        
+        return np.sum(predict==self.classes[picked])/len(picked)
+            
+   
+
+
+
+
 
