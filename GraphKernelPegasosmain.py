9452435aa0035830e36ed72f59152142fe0f9386
kota <kota160815@gmail.com>
2025-06-03 13:45:04 +0900

    update

--- GraphKernelPegasosmain.py
+++ GraphKernelPegasosmain.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pegasos_evaluation as pg
 import data
+import time
 
 #-------------------
 # Make data
@@ -12,9 +13,14 @@ myData.makeData(dataType=int(input("data type =>")))
 #-------------------
 # Learning and evaluation.
 print("lamda,acc")
-for lamda in [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0]:
+for lamda in [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10, 100]:
     Pegasos = pg.Pegasos(myData.graphs, myData.classes, 100, lamda)
+
+    start = time.time()
     alpha = Pegasos.train()
+    end = time.time()
+    print(f"処理時間: {end - start:.4f} 秒")
+    
     acc = Pegasos.accuracy(alpha)
     print(lamda,",",acc)
 
